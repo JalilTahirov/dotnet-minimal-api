05c4a145ee07b5e8dd24d132fb0438854ebf4275
Jalil Tahirov <Jalil.Tahirov@dominos.co.uk>
2024-04-10 13:22:47 +0400

    added gitignore

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -17,7 +17,7 @@ app.UseSwaggerUI(c =>
      c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
    });
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!"); // this is for reference
 app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
 app.MapGet("/pizzas",() => PizzaDB.GetPizzas());
 app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
