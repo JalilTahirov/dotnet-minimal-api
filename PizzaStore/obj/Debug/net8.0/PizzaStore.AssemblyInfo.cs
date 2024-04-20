de1ccef999fc0b827e1e4e91ad5802436eb66a7a
Jalil Tahirov <Jalil.Tahirov@dominos.co.uk>
2024-04-20 11:36:51 +0400

    Entity Framework Was Added

--- PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b7cf77ac2bfccaa4cf8c3010e5f0c9b6da36a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e32e06019c06ed2ca472f15fa2c6aade53913b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
